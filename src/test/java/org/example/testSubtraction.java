d9e6474749e50522a364498a5a8257cbc265667b
Junkai Wang <2971106303@qq.com>
2023-10-01 10:06:45 +0800

    sub

--- src/test/java/org/example/testSubtraction.java
+++ src/test/java/org/example/testSubtraction.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class testSubtraction {
+    Calc c = new Calc();
     void subtract(){
         assertEquals(2, c.subtract(4,2));
     }
